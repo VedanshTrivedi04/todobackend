05c392f1265ea9db4d809077680d0cdb25daac0d
VedanshTrivedi04 <165897916+VedanshTrivedi04@users.noreply.github.com>
2026-03-06 21:27:26 +0530

    Implement CORS middleware for frontend access
    Add CORS middleware to FastAPI app

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from database import engine
 from  routers import auth,todos,admin,user
 
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+origins = [
+    "http://localhost:5173",   # your React/Vite frontend
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
