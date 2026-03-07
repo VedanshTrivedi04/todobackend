5ff9240e313dd5bb26bbe4c841a4e37472e7bed6
VedanshTrivedi04 <165897916+VedanshTrivedi04@users.noreply.github.com>
2026-03-07 12:11:40 +0530

    Extend token expiration to 60 days

--- routers/auth.py
+++ routers/auth.py
@@ -111,5 +111,5 @@ async def login_for_token(form_data:Annotated[OAuth2PasswordRequestForm,Depends(
     if not user:
          raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                         detail="could not a vlid user ")
-    token= create_access_token(user.username,user.id,user.role,timedelta(minutes=20))
+    token= create_access_token(user.username,user.id,user.role,timedelta(days=60))
     return {'access_token': token,'token_type':'bearer'}
